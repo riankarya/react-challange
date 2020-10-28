304988fc1c34835f86e14a96162a5d63ab7f700c
riankarya <riankaryamarpaung@gmail.com>
2020-10-28 15:51:57 +0700

    react hooks and function components

--- react-challenge/src/components/DotaCard.jsx
+++ react-challenge/src/components/DotaCard.jsx
@@ -1,40 +1,34 @@
 import React from 'react'
 import { Button, Card } from 'react-bootstrap';
 
-class DotaCard extends React.Component {
-    attribute() {
-      switch (this.props.primary_attr) {
-        case "str":
-          return "Strength"
-        case "agi":
-          return "Agility"
-        case "int":
-          return "Intelligence"
-        default:
-          console.log(this.props, 'ASUPPPPPPP');
-          return "asup"
-      }
-    }
-    render() {
-      return <div>
-      <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src={`http://cdn.dota2.com/apps/dota2/images/heroes/${this.props.img_url}_vert.jpg
-`} />
-  <Card.Body>
-    <Card.Title>{this.props.name}</Card.Title>
-    <Card.Text>
-    Primary Attribute: {this.attribute()}
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
-      </div>
-//       return <Card>
-//         <img src={`http://cdn.dota2.com/apps/dota2/images/heroes/${this.props.img_url}_vert.jpg
-// `} alt="" srcset=""/>
-// <h1>Hello, {this.props.name}</h1>;
-//         </Card>
+const DotaCard = (props) => {
+  const attribute = () => {
+    switch (props.primary_attr) {
+      case "str":
+        return "Strength"
+      case "agi":
+        return "Agility"
+      case "int":
+        return "Intelligence"
+      default:
+        return ""
     }
   }
+  return (
+      <div>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={`http://cdn.dota2.com/apps/dota2/images/heroes/${props.img_url}_vert.jpg
+        `} />
+          <Card.Body>
+            <Card.Title>{props.name}</Card.Title>
+            <Card.Text>
+            Primary Attribute: {attribute()}
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </div>
+  )
+}
 
 export default DotaCard
